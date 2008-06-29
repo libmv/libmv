7262496518edc9538a82f9b896d9c68d58e4f8bd
Darius Blaszyk <dblaszyk@gmail.com>
2008-06-29 23:00:52 +0000

    updated create_patches.sh to reflect current status initial import of blender patch, gives a segfault but it's WIP so be warned

--- contrib/blender/create_patches.sh
+++ contrib/blender/create_patches.sh
@@ -1,2 +1,5 @@
 #create the blender-libmv patch
-svn diff ../../../../source/blender/src/buttons_editing.c ../../../../source/blender/src/buttons_editing.c > blender_libmv.diff
+svn diff ../../../../source/blender/src/buttons_editing.c \
+         ../../../../source/blender/include/butspace.h \
+         ../../../../source/blender/makesdna/DNA_libmv_types.h \
+         > blender_libmv.diff
