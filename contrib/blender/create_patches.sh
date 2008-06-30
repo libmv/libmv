adaae1f29a23141b2e73a436e5f04d07920ac009
Darius Blaszyk <dblaszyk@gmail.com>
2008-06-30 23:33:11 +0000

    cleanup of DNA_libmv_types.h added libmvConfig struct to DNA_userdef_types.h and makesdna.c so it gets saved to .blend file

--- contrib/blender/create_patches.sh
+++ contrib/blender/create_patches.sh
@@ -2,4 +2,6 @@
 svn diff ../../../../source/blender/src/buttons_editing.c \
          ../../../../source/blender/include/butspace.h \
          ../../../../source/blender/makesdna/DNA_libmv_types.h \
+         ../../../../source/blender/makesdna/DNA_userdef_types.h \
+         ../../../../source/blender/makesdna/intern/makesdna.c \
          > blender_libmv.diff
