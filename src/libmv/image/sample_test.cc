4777851a5abc94ddbf06991fecf68864bf13df6d
Keir Mierle <mierle@gmail.com>
2008-08-03 19:31:17 +0000

    Add a test for multichannel downsample.

--- src/libmv/image/sample_test.cc
+++ src/libmv/image/sample_test.cc
@@ -60,4 +60,30 @@ TEST(Image, DownsampleBy2) {
   EXPECT_FLOAT_EQ(6./4., resampled_image(0, 0));
 }
 
+TEST(Image, DownsampleBy2MultiChannel) {
+  Array3Df image(2, 2, 3);
+  image(0, 0, 0) = 0;
+  image(0, 1, 0) = 1;
+  image(1, 0, 0) = 2;
+  image(1, 1, 0) = 3;
+
+  image(0, 0, 1) = 5;
+  image(0, 1, 1) = 6;
+  image(1, 0, 1) = 7;
+  image(1, 1, 1) = 8;
+
+  image(0, 0, 2) = 9;
+  image(0, 1, 2) = 10;
+  image(1, 0, 2) = 11;
+  image(1, 1, 2) = 12;
+
+  Array3Df resampled_image;
+  DownsampleChannelsBy2(image, &resampled_image);
+  ASSERT_EQ(1, resampled_image.Height());
+  ASSERT_EQ(1, resampled_image.Width());
+  ASSERT_EQ(3, resampled_image.Depth());
+  EXPECT_FLOAT_EQ((0+1+2+3)/4.,    resampled_image(0, 0, 0));
+  EXPECT_FLOAT_EQ((5+6+7+8)/4.,    resampled_image(0, 0, 1));
+  EXPECT_FLOAT_EQ((9+10+11+12)/4., resampled_image(0, 0, 2));
+}
 }  // namespace
