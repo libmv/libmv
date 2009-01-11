ed3ab2d17485f9a7b901203ef7d6bb73021aa4a1
Keir Mierle <mierle@gmail.com>
2009-01-11 22:46:02 +0000

    Add more matrix testing helpers.

--- src/testing/testing.h
+++ src/testing/testing.h
@@ -26,12 +26,15 @@
 
 #define EXPECT_MATRIX_NEAR(a, b, tolerance) \
 do { \
-  ASSERT_EQ(a.rows(), b.rows()) << "Matrix rows don't match:"; \
-  ASSERT_EQ(a.cols(), b.cols()) << "Matrix cols don't match."; \
-  for (int r = 0; r < a.rows(); ++r) { \
-    for (int c = 0; c < a.cols(); ++c) { \
-      EXPECT_NEAR(a(r, c), b(r, c), tolerance) \
-        << "r=" << r << ", c=" << c << "."; \
+  bool dims_match = (a.rows() == b.rows()) && (a.cols() == b.cols()); \
+  EXPECT_EQ(a.rows(), b.rows()) << "Matrix rows don't match."; \
+  EXPECT_EQ(a.cols(), b.cols()) << "Matrix cols don't match."; \
+  if (dims_match) { \
+    for (int r = 0; r < a.rows(); ++r) { \
+      for (int c = 0; c < a.cols(); ++c) { \
+        EXPECT_NEAR(a(r, c), b(r, c), tolerance) \
+          << "r=" << r << ", c=" << c << "."; \
+      } \
     } \
   } \
 } while(false);
@@ -42,6 +45,21 @@ do { \
     for (int c = 0; c < a.cols(); ++c) { \
       EXPECT_NEAR(0.0, a(r, c), tolerance) \
         << "r=" << r << ", c=" << c << "."; \
+    } \
+  } \
+} while(false);
+
+#define EXPECT_MATRIX_EQ(a, b) \
+do { \
+  bool dims_match = (a.rows() == b.rows()) && (a.cols() == b.cols()); \
+  EXPECT_EQ(a.rows(), b.rows()) << "Matrix rows don't match."; \
+  EXPECT_EQ(a.cols(), b.cols()) << "Matrix cols don't match."; \
+  if (dims_match) { \
+    for (int r = 0; r < a.rows(); ++r) { \
+      for (int c = 0; c < a.cols(); ++c) { \
+        EXPECT_EQ(a(r, c), b(r, c)) \
+          << "r=" << r << ", c=" << c << "."; \
+      } \
     } \
   } \
 } while(false);
