2c52b3d34d4bef93f412804a9a353aa7a80608ff
Keir Mierle <mierle@gmail.com>
2009-04-19 03:25:12 +0000

    Add an even simpler scene graph; closer to a transform graph.

---
+++ src/libmv/scene_graph/scene_graph_simple.h
@@ -0,0 +1,175 @@
+// Copyright (c) 2009 libmv authors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#ifndef LIBMV_SCENE_GRAPH_SCENE_GRAPH_SIMPLE_H
+#define LIBMV_SCENE_GRAPH_SCENE_GRAPH_SIMPLE_H
+
+#include <map>
+
+#include "libmv/numeric/numeric.h"
+#include "libmv/logging/logging.h"
+
+namespace libmv {
+namespace scene {
+
+using std::string;
+
+// TODO(keir): Consider eliminating the global transform. OpenGL already has
+// its own transform stack; why is it useful to have a global transform for
+// each object?
+
+/**
+ * The simplest possible scene graph that could possibly work for displaying a
+ * two view reconstruction. Performance is utterly ignored in favor of code
+ * simplicity and interface ease of use.
+ */
+template<class Object>
+class Node {
+  typedef Node<Object> NodeT;
+  typedef std::map<string, NodeT *> ChildMap;
+ public:
+  virtual ~Node() {}
+  Node() {
+    transform_ = Mat4::Identity(4, 4);
+    current_ = Mat4::Identity(4, 4);
+  }
+  Node(const string &name, Object *object) {
+    name_ = name;
+    object_ = object;
+    transform_ = Mat4::Identity(4, 4);
+    current_ = Mat4::Identity(4, 4);
+  }
+
+  NodeT *GetParent() const { return parent_; }
+  void SetParent(NodeT *new_parent) {
+    if (parent_ != NULL) {
+      parent_->RemoveChild(this);
+    }
+    parent_ = new_parent;
+    if (new_parent != NULL) {
+      new_parent->AddChild(this);
+    }
+    UpdateGlobalTransform();
+  }
+
+  NodeT *GetChild(const string &child_name) const {
+    typename ChildMap::const_iterator it = children_.find(child_name);
+    return (it == children_.end()) ? NULL : it->second;
+  }
+
+  void AddChild(NodeT *child) {
+    // Children must have unique names.
+    NodeT *existing_child = GetChild(child->GetName());
+    if (existing_child == NULL) {
+      children_[child->GetName()] = child;
+      child->SetParent(this);
+    } else if (child == existing_child) {
+      LOG(WARNING) << "Ignoring attempt to add child node <" << child->GetName()
+                   << "> to parent node <" << GetName() << " multiple times";
+    } else {
+      LOG(WARNING) << "Ignoring attempt to add child node <" << child->GetName()
+                   << "> to parent node <" << GetName()
+                   << "> which already has a node with this name "
+                   << "(which doesn't match the node to be added";
+    }
+  }
+
+  void RemoveChild(NodeT *child) {
+    typename ChildMap::iterator it = children_.find(child->GetName());
+    if (it != children_.end()) {
+      children_.erase(it);
+    }
+  }
+
+  const string &GetName() const    { return name_; }
+  void SetName(const string &name) { name_ = name; }
+
+  const Mat4 &GetLocalTransform()  const { return transform_; }
+  const Mat4 &GetGlobalTransform() const { return current_;   }
+
+  Object *GetObject() const { return object_; }
+  void SetObject(Object *object) { object_ = object; }
+  
+  // The transform to move from the coordinate space of parent to the
+  // coordinate space of this object.
+  void SetTransform(const Mat4 &transform) {
+    transform_ = transform;
+    UpdateGlobalTransform();
+  }
+  void TransformBy(const Mat4 &transform) {
+    transform_ = transform_ * transform;
+    UpdateGlobalTransform();
+  }
+
+  class iterator {
+    friend class Node<Object>;
+   public:
+    NodeT * operator*() const {
+      return it_->second;
+    }
+    bool operator!=(const iterator &other) {
+      return it_ != other.it_;
+    }
+    void operator++() {
+      ++it_;
+    }
+   private:
+    iterator(const typename ChildMap::iterator &it) {
+      it_ = it;
+    }
+    typename ChildMap::iterator it_;
+  };
+  // Iterate over the children of this node.
+  iterator begin() {
+    return iterator(children_.begin());
+  }
+  iterator end() {
+    return iterator(children_.end());
+  }
+
+ private:
+  void UpdateGlobalTransform() {
+    if (parent_) {
+      current_ = parent_->GetGlobalTransform() * transform_;
+    } else {
+      current_ = transform_;
+    }
+    this->UpdateChildren();
+  }
+  void UpdateChildren() {
+    for (iterator it = begin(); it != end(); ++it) {
+      (*it)->UpdateGlobalTransform();
+    }
+  }
+
+  // Transform from the parents space to this object's space.
+  Mat4 transform_;
+  // Transform from root object space into this objects coordinate space.
+  Mat4 current_;
+  string name_;
+  NodeT *parent_;
+  ChildMap children_;
+  Object *object_;
+};
+
+}  // namespace scene
+}  // namespace libmv
+
+#endif  // LIBMV_SCENE_GRAPH_SCENE_GRAPH_SIMPLE_H
---
+++ src/libmv/scene_graph/scene_graph_simple_test.cc
@@ -0,0 +1,63 @@
+// Copyright (c) 2009 libmv authors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#include <string>
+
+#include "libmv/scene_graph/scene_graph_simple.h"
+#include "libmv/logging/logging.h"
+#include "testing/testing.h"
+
+using libmv::scene::Node;
+
+TEST(SceneGraph, IteratingOverAddedNodes) {
+  Node<int> scene("root node", new int(0));
+  scene.AddChild(new Node<int>("bar", new int(5)));
+  scene.AddChild(new Node<int>("zap", new int(10)));
+  scene.AddChild(new Node<int>("foo", new int(4)));
+
+  const char * expected_names[] = { "bar", "foo", "zap" };
+  int expected_values[] = { 5, 4, 10 };
+  int i = 0;
+  for (Node<int>::iterator it = scene.begin(); it != scene.end(); ++it, ++i) {
+    EXPECT_EQ(expected_names[i], (*it)->GetName());
+    EXPECT_EQ(expected_values[i], *(*it)->GetObject());
+    EXPECT_EQ(&scene, (*it)->GetParent());
+  }
+  delete scene.GetObject();
+  for (Node<int>::iterator it = scene.begin(); it != scene.end(); ++it) {
+    delete (*it)->GetObject();
+  }
+}
+
+TEST(SceneGraph, GetChildReturnsNullIfNotFound) {
+  Node<int> scene("root node", new int(0));
+
+  const char * test_names[] = { "bar", "foo", "zap" };
+  for (int i = 0; i < 3; ++i) {
+    EXPECT_TRUE(NULL == scene.GetChild(test_names[i]));
+    Node<int> *node = new Node<int>(test_names[i], new int(10));
+    scene.AddChild(node);
+    EXPECT_EQ(node, scene.GetChild(test_names[i]));
+  }
+  delete scene.GetObject();
+  for (Node<int>::iterator it = scene.begin(); it != scene.end(); ++it) {
+    delete (*it)->GetObject();
+  }
+}
