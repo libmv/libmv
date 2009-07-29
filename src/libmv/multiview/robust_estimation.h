bc0e3d2823aa53b23d72d2da834b02f5f831e055
Pierre Moulon <pmoulon@gmail.com>
2009-07-29 14:08:25 +0000

    - Use const reference instead of TMat copy. Consequence : Minor speedup.

--- src/libmv/multiview/robust_estimation.h
+++ src/libmv/multiview/robust_estimation.h
@@ -1,15 +1,15 @@
 // Copyright (c) 2007, 2008 libmv authors.
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to
 // deal in the Software without restriction, including without limitation the
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -72,7 +72,7 @@ double SigmaApprox(int num_inliers, int num_sampled, int num_total) {
 
 template<typename Model, typename TMat, typename Fitter, typename Classifier,
          typename CostFunction>
-Model Estimate(TMat &samples, Fitter fitter, Classifier classifier,
+Model Estimate(const TMat &samples, Fitter fitter, Classifier classifier,
                CostFunction cost_function, std::vector<int> *inliers = NULL) {
   uint iteration = 0;
   uint max_iterations = 100;
