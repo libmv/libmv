0bc03ef7a35648037c9f914874b57351c4e8e521
Pau Gargallo <pau.gargallo@gmail.com>
2009-08-27 15:54:24 +0000

    Formating image_drawing. Input output changes comming.

--- src/libmv/image/image_drawing.h
+++ src/libmv/image/image_drawing.h
@@ -29,120 +29,109 @@ namespace libmv {
 // Bresenham approach do not allow to draw concentric circle without holes.
 // So it's better the use the Andres method.
 // http://fr.wikipedia.org/wiki/Algorithme_de_tracé_de_cercle_d'Andres.
-template < class Image , class Color >
-void DrawCircle( int x, int y, int radius, Image & im, const Color & col)
-{
-  if( im.Contains( y + radius, x + radius ) && im.Contains( y + radius, x - radius) &&
-        im.Contains( y - radius, x + radius ) && im.Contains( y - radius, x - radius) )
-    {
-     /*
-      // Naive Approach (use x_center + radius * cos(angle) and y_center + radius * sin(angle).
-      // One optimization :
-      // Draw North and South pixel (use axial symmetry to minimize the sin cos computation).
-      // Use demi-perimeter formula to know how much point we must rasterize to perform a continuous line to make the circle
-      for(float k=0.0f; k < 3.14f; k+= 3.14f/(3.14f*radius))
-      {
-        const int sinus = sin( k ) * radius;
-        const int cosinus = cos( k ) *radius;
-
-        im( y + sinus, x + cosinus ) = col;
-				im( y - sinus, x + cosinus ) = col;
-      }*/
-
-      int x1 = 0;
-      int y1 = radius;
-      int d = radius-1;
-      while(y1>=x1)
-      {
-        // Draw the point for each octant.
-        im( y1+y,  x1+x)=col;
-        im( x1+y,  y1+x)=col;
-        im( y1+y, -x1+x)=col;
-        im( x1+y, -y1+x)=col;
-        im(-y1+y,  x1+x)=col;
-        im(-x1+y,  y1+x)=col;
-        im(-y1+y, -x1+x)=col;
-        im(-x1+y, -y1+x)=col;
-	      if( d >= 2*x1 ) {
-		      d = d-2*x1-1;
-		      x1 += 1;
-	      }
-	      else  {
-          if( d <= 2*(radius-y1) )  {
-            d = d+2*y1-1;
-            y1 -= 1;
-          }
-          else  {
-            d = d+2*(y1-x1-1);
-            y1 -=1;
-            x1 +=1;
-          }
-	      }
+template <class Image, class Color>
+void DrawCircle(int x, int y, int radius, Image &im, const Color &col) {
+  if (  im.Contains(y + radius, x + radius)
+     && im.Contains(y + radius, x - radius)
+     && im.Contains(y - radius, x + radius)
+     && im.Contains(y - radius, x - radius)) {
+    int x1 = 0;
+    int y1 = radius;
+    int d = radius - 1;
+    while (y1 >= x1) {
+      // Draw the point for each octant.
+      im( y1 + y,  x1 + x) = col;
+      im( x1 + y,  y1 + x) = col;
+      im( y1 + y, -x1 + x) = col;
+      im( x1 + y, -y1 + x) = col;
+      im(-y1 + y,  x1 + x) = col;
+      im(-x1 + y,  y1 + x) = col;
+      im(-y1 + y, -x1 + x) = col;
+      im(-x1 + y, -y1 + x) = col;
+      if (d >= 2 * x1) {
+        d = d - 2 * x1 - 1;
+        x1 += 1;
+      } else {
+        if (d <= 2 * (radius - y1)) {
+          d = d + 2 * y1 - 1;
+          y1 -= 1;
+        } else  {
+          d = d + 2 * (y1 - x1 - 1);
+          y1 -= 1;
+          x1 += 1;
+        }
       }
     }
+  }
 }
 
 // Bresenham algorithm
-template < class Image , class Color >
-void DrawLine(int xa, int ya, int xb, int yb, Image & im, const Color & col)
-{
-  if( !im.Contains( ya, xa ) && !im.Contains( yb, xb ))
+template <class Image, class Color>
+void DrawLine(int xa, int ya, int xb, int yb, Image &im, const Color &col) {
+  // TODO(pau): Deal with start and end points outside the image.
+  if (!im.Contains(ya, xa) && !im.Contains(yb, xb))
     return;
 
   int xbas, xhaut, ybas, yhaut;
   // Check the condition ybas < yhaut.
-  if(ya<=yb)
-  {
-    xbas=xa;  ybas=ya;
-    xhaut=xb; yhaut=yb;
-  }
-  else
-  {
-    xbas=xb; xhaut=xa;
-    ybas=yb; yhaut=ya;
+  if (ya <= yb) {
+    xbas = xa;
+    ybas = ya;
+    xhaut = xb;
+    yhaut = yb;
+  } else {
+    xbas = xb;
+    ybas = yb;
+    xhaut = xa;
+    yhaut = ya;
   }
 
   // Initialize slope.
-  int x,y,dx,dy,incrmX,incrmY,dp,N,S;
-  dx = xhaut-xbas;
-  dy = yhaut-ybas;
-  if (dx>0) // If xhaut>xbas we will increment X.
+  int x, y, dx, dy, incrmX, incrmY, dp, N, S;
+  dx = xhaut - xbas;
+  dy = yhaut - ybas;
+  if (dx > 0) { // If xhaut > xbas we will increment X.
     incrmX = 1;
-  else
-  {
+  } else {
     incrmX = -1; // else we will decrement X.
     dx *= -1;
   }
-  if (dy>0) // Positive slope will increment X.
+  if (dy > 0) { // Positive slope will increment X.
     incrmY = 1;
-  else // Negative slope.
+  } else {      // Negative slope.
     incrmY = -1;
-  if (dx>=dy)
-  {
-    dp=2*dy-dx; S=2*dy; N=2*(dy-dx);
-    y=ybas; x=xbas;
-    while(x!=xhaut)
-    {
-      im(y,x) = col;
-      x+=incrmX;
-      if (dp<=0) // Go in direction of the South Pixel.
-        dp += S;
-      else // Go to the North.
-      { dp += N; y+=incrmY; }
-    }
   }
-  else
-  {
-    dp=2*dx-dy; S=2*dx; N=2*(dx-dy);
-    x=xbas; y=ybas;
-    while(y<yhaut)
-    {
+  if (dx >= dy) {
+    dp = 2 * dy - dx;
+    S = 2 * dy;
+    N = 2 * (dy - dx);
+    y = ybas;
+    x = xbas;
+    while (x != xhaut) {
       im(y,x) = col;
-      y+=incrmY;
-      if (dp<=0) // Go in direction of the South Pixel.
+      x += incrmX;
+      if (dp <= 0) { // Go in direction of the South Pixel.
         dp += S;
-      else // Go to the North.
-      { dp += N; x+=incrmX; }
+      } else {       // Go to the North.
+        dp += N;
+        y+=incrmY;
+      }
+    }
+  } else {
+    dp = 2 * dx - dy;
+    S = 2 * dx;
+    N = 2 * (dx - dy);
+    x = xbas;
+    y = ybas;
+    while (y < yhaut) {
+      im(y,x) = col;
+      y += incrmY;
+      if (dp <= 0) { // Go in direction of the South Pixel.
+        dp += S;
+      } else {       // Go to the North.
+        dp += N;
+        x += incrmX;
+      }
     }
   }
   im(y,x) = col;
