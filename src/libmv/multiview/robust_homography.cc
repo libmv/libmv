aa3bec2ad8500279f01eadd4f8fbb59b4f3dc6cf
Pierre Moulon <pmoulon@gmail.com>
2009-10-28 21:31:57 +0000

    Add robust_homography files (4point and 2point). (2 point is yet experimental). Test will be added later.

---
+++ src/libmv/multiview/robust_homography.cc
@@ -0,0 +1,61 @@
+// Copyright (c) 2007, 2008 libmv authors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#include <cstdio>
+#include <vector>
+
+#include "libmv/multiview/robust_homography.h"
+#include "libmv/multiview/homography_kernel.h"
+#include "libmv/multiview/panography_kernel.h"
+#include "libmv/multiview/robust_estimation.h"
+#include "libmv/numeric/numeric.h"
+#include "libmv/logging/logging.h"
+
+namespace libmv {
+
+double HomographyFromCorrespondences4PointRobust(const Mat &x1,
+                                                 const Mat &x2,
+                                                 double max_error,
+                                                 Mat3 *H,
+                                                 vector<int> *inliers) {
+  // The threshold is on the sum of the squared errors in the two images.
+  // Actually, Sampson's approximation of this error.
+  double threshold = 2 * Square(max_error);
+  typedef homography::kernel::Kernel KernelH;
+  KernelH kernel(x1, x2);
+  *H = Estimate(kernel, MLEScorer<KernelH>(threshold), inliers);
+  return 0.0;  // This doesn't mean much for the robust case.
+}
+
+double HomographyFromCorrespondences2PointRobust(const Mat &x1,
+                                                  const Mat &x2,
+                                                  double max_error,
+                                                  Mat3 * H,
+                                                  vector<int> *inliers) {
+  // The threshold is on the sum of the squared errors in the two images.
+  // Actually, Sampson's approximation of this error.
+  double threshold = 2 * Square(max_error);
+  typedef panography::kernel::Kernel Kernel;
+  Kernel kernel(x1, x2);
+  *H = Estimate(kernel, MLEScorer<Kernel>(threshold), inliers);
+  return 0.0;  // This doesn't mean much for the robust case.
+}
+
+}  // namespace libmv
---
+++ src/libmv/multiview/robust_homography.h
@@ -0,0 +1,48 @@
+// Copyright (c) 2009 libmv authors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#ifndef LIBMV_MULTIVIEW_ROBUST_HOMOGRAPHY_H_
+#define LIBMV_MULTIVIEW_ROBUST_HOMOGRAPHY_H_
+
+#include "libmv/base/vector.h"
+#include "libmv/numeric/numeric.h"
+
+namespace libmv {
+
+// Estimate robustly the homography matrix between two dataset of 2D point
+// (image coords space). The fundamental solver relies on the 4 point solution.
+double HomographyFromCorrespondences4PointRobust(
+    const Mat &x1,
+    const Mat &x2,
+    double max_error,
+    Mat3 *H,
+    vector<int> *inliers = NULL);
+
+// Estimate robustly the fundamental matrix between two dataset of 2D point
+// (image coords space). The fundamental solver relies on the 2 point solution.
+double HomographyFromCorrespondences2PointRobust(const Mat &x1,
+                                                  const Mat &x2,
+                                                  double max_error,
+                                                  Mat3 * H,
+                                                  vector<int> *inliers);
+
+} // namespace libmv
+
+#endif  // LIBMV_MULTIVIEW_ROBUST_FUNDAMENTAL_H_
