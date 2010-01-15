51357755abe17adede5bbf7ccb3ba94c4ea701a9
Pierre Moulon <pmoulon@gmail.com>
2010-01-15 16:04:17 +0000

    Add xcas source code to obtain panography shared Focal polynomial solver.

--- src/libmv/multiview/panography_coeffs.py
+++ src/libmv/multiview/panography_coeffs.py
@@ -1,3 +1,7 @@
+# Minimal Solutions for Panoramic Stitching. M. Brown, R. Hartley and D. Nister. 
+# International Conference on Computer Vision and Pattern Recognition
+# (CVPR2007). Minneapolis, June 2007.
+
 import sympy
 
 f2, a12, a1, a2, b12, b1, b2 = sympy.symbols('f2 a12 a1 a2 b12 b1 b2')
@@ -13,3 +17,13 @@ print '  double P[4] = {'
 for i in range(4):
     print '    ', sympy.printing.ccode(d[(i,)])
 print '  }'
+
+# As long as Python code do not return the correct value
+# I prefer use "Xcas Computer Algebra System" 
+# http://www-fourier.ujf-grenoble.fr/~parisse/giac.html
+
+# Solution for the focal length of a pair of images from a rotating camera.
+# comment(" Define the base equation that share the Focal length 'f' ")
+# FocalSolver:=(a12+f)^2 / (( a1+f)*(a2+f)) - (b12+f)^2 / ((b1+f)*(b2+f));
+# comment(" Solve equation=0 with the unknow 'f' ")
+# solve(FocalSolver=0,f);
