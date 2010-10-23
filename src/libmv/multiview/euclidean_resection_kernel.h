ec6cb72d86390bff4d9ffba8d6b7ad96f480b59e
Julien Michot <julien.michot.fr@gmail.com>
2010-10-23 12:24:16 +0000

    Add missing files (kernel, kernel_test)

---
+++ src/libmv/multiview/euclidean_resection_kernel.h
@@ -0,0 +1,75 @@
+// Copyright (c) 2009 libmv authors.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#ifndef LIBMV_MULTIVIEW_EUCLIDEAN_RESECTION_KERNEL_H
+#define LIBMV_MULTIVIEW_EUCLIDEAN_RESECTION_KERNEL_H
+
+#include "libmv/base/vector.h"
+#include "libmv/logging/logging.h"
+#include "libmv/multiview/euclidean_resection.h"
+#include "libmv/numeric/numeric.h"
+
+namespace libmv {
+namespace euclidean_resection {
+namespace kernel {
+
+class Kernel {
+ public:
+  typedef Mat34 Model;
+  enum { MINIMUM_SAMPLES = 5 };
+  // TODO(julien) avoid the copy of x_camera: create a new Kernel ?
+  Kernel(const Mat2X &x_camera, const Mat3X &X) : x_camera_(x_camera), X_(X) {
+    CHECK(x_camera.cols() == X.cols());
+  }
+  Kernel(const Mat2X &x_image, const Mat3X &X, const Mat3 &K) : X_(X) {
+    CHECK(x_image.cols() == X.cols());
+    // Conversion from image coordinates to normalized camera coordinates 
+    EuclideanToNormalizedCamera(x_image, K, &x_camera_);
+  }
+  void Fit(const vector<int> &samples, vector<Model> *models) const {
+    Mat2X x = ExtractColumns(x_camera_, samples);
+    Mat3X X = ExtractColumns(X_, samples);
+    Mat34 P;
+    Mat3 K; K.setIdentity();
+    Mat3 R;
+    Vec3 t;
+    EuclideanResection(x, X, &R, &t);
+    P_From_KRt(K, R, t, &P);
+    models->push_back(P);
+  }
+  double Error(int sample, const Model &model) const {
+    Mat3X X = X_.col(sample);
+    Mat2X error = Project(model, X) - x_camera_.col(sample);
+    return error.col(0).norm();
+  }
+  int NumSamples() const {
+    return x_camera_.cols();
+  }
+ private:
+  // x_camera_ contains the normalized camera coordinates 
+        Mat2X  x_camera_;
+  const Mat3X &X_;
+};
+
+}  // namespace kernel
+}  // namespace resection
+}  // namespace libmv
+
+#endif  // LIBMV_MULTIVIEW_EUCLIDEAN_RESECTION_KERNEL_H
---
+++ src/libmv/multiview/euclidean_resection_kernel_test.cc
@@ -0,0 +1,64 @@
+// Copyright (c) 2009 libmv authors.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
+#include "libmv/logging/logging.h"
+#include "libmv/multiview/projection.h"
+#include "libmv/multiview/euclidean_resection_kernel.h"
+#include "libmv/multiview/robust_estimation.h"
+#include "libmv/multiview/test_data_sets.h"
+#include "libmv/numeric/numeric.h"
+#include "testing/testing.h"
+
+namespace libmv {
+namespace {
+
+using libmv::euclidean_resection::kernel::Kernel;
+
+TEST(EuclideanResectionKernel, RobustEuclideanResection) {
+  int nviews = 5;
+  int npoints = 60;
+  int noutliers = 0.3*npoints;
+  double threshold_inlier = Square(0.2);
+  NViewDataSet d = NRealisticCameras(nviews, npoints);
+  for (int i = 0; i < nviews; ++i) {
+    Mat2X x = d.x[i];
+    
+    // Now make 30% of the points in x totally wrong.
+    x.block(0, 0, 2, noutliers).setRandom();
+
+    Kernel kernel(x, d.X, d.K[i]);
+    vector<int> inliers;
+    Mat34 P = Estimate(kernel, 
+                       MLEScorer<Kernel>(threshold_inlier), 
+                       &inliers);
+    Mat34 P_expected = d.K[i].inverse() * d.P(i);
+    EXPECT_MATRIX_PROP(P_expected, P, 3e-8);
+    
+    // Make sure inliers were classified properly.
+    for (int i = 0; i < inliers.size(); ++i) {
+      LOG(INFO) << inliers[i];
+      EXPECT_EQ(i + noutliers, inliers[i]);  // 0..19 are outliers.
+    }
+    EXPECT_EQ(npoints-noutliers, inliers.size());
+  }
+}
+
+}  // namespace
+}  // namespace libmv
