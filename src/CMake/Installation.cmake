313f8286a7c1a87f13e5314e739aa2d27b5a90a3
Julien Michot <julien.michot.fr@gmail.com>
2011-03-07 21:29:08 +0000

    Add a FindLibmv CMake file and installation rules for third party libraries.

--- src/CMake/Installation.cmake
+++ src/CMake/Installation.cmake
@@ -1,20 +1,26 @@
 #Install macro for libmv libraries
 MACRO (LIBMV_INSTALL_LIB NAME)
-  # install headers
-  INSTALL(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-          DESTINATION ${LIBMV_HEADERS_OUTPUT_DIR}/libmv
-          FILES_MATCHING PATTERN "*.h"
-                         PATTERN "*.hpp"
-                         PATTERN ".svn" EXCLUDE
-        )
-  # install libraries
   INSTALL(TARGETS ${NAME} ${NAME}
     LIBRARY DESTINATION ${LIBMV_LIBRARY_OUTPUT_DIR}
     ARCHIVE DESTINATION ${LIBMV_LIBRARY_OUTPUT_DIR}
   )
 ENDMACRO (LIBMV_INSTALL_LIB)
 
-#Install macro for libmv binaries
+# Install all libmv and third parties headers
+MACRO (LIBMV_INSTALL_ALL_HEADERS)
+  INSTALL(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+          DESTINATION ${LIBMV_HEADERS_OUTPUT_DIR}
+          FILES_MATCHING PATTERN "*"
+                         PATTERN ".svn" EXCLUDE
+                         PATTERN "*.cpp" EXCLUDE
+                         PATTERN "*.cc" EXCLUDE
+                         PATTERN "*.c" EXCLUDE
+                         PATTERN "*~" EXCLUDE
+                         PATTERN "CMakeLists.txt" EXCLUDE
+        )
+ENDMACRO (LIBMV_INSTALL_ALL_HEADERS)
+
+# Install macro for libmv binaries
 MACRO (LIBMV_INSTALL_EXE NAME)
   # install libraries
   INSTALL(TARGETS ${NAME}
@@ -24,29 +30,13 @@ ENDMACRO (LIBMV_INSTALL_EXE)
 
 #Install macro for third parties libraries
 MACRO (LIBMV_INSTALL_THIRD_PARTY_LIB NAME)
-  # install libraries
   INSTALL(TARGETS ${NAME} ${NAME}
     LIBRARY DESTINATION ${LIBMV_LIBRARY_OUTPUT_DIR}
     ARCHIVE DESTINATION ${LIBMV_LIBRARY_OUTPUT_DIR}
   )
 ENDMACRO (LIBMV_INSTALL_THIRD_PARTY_LIB)
 
-#Install macro for third parties headers
-MACRO (LIBMV_INSTALL_THIRD_PARTY_HEADERS)
-  # install headers
-  INSTALL(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-          DESTINATION ${LIBMV_HEADERS_OUTPUT_DIR}/third_party
-          FILES_MATCHING PATTERN "*"
-                         PATTERN ".svn" EXCLUDE
-                         PATTERN "*.cpp" EXCLUDE
-                         PATTERN "*.cc" EXCLUDE
-                         PATTERN "*.c" EXCLUDE
-                         PATTERN "CMakeLists.txt" EXCLUDE
-        )
-ENDMACRO (LIBMV_INSTALL_THIRD_PARTY_HEADERS)
-
-
-# uninstall target
+# Uninstall target
 configure_file(
     "${CMAKE_CURRENT_SOURCE_DIR}/CMake/cmake_uninstall.cmake.in"
     "${CMAKE_CURRENT_BINARY_DIR}/../cmake_uninstall.cmake"
@@ -54,3 +44,8 @@ configure_file(
 
 add_custom_target(uninstall
     COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/../cmake_uninstall.cmake)
+
+
+INSTALL(FILES CMake/modules/FindLibmv.cmake
+        DESTINATION "${CMAKE_ROOT}/Modules/"
+        PERMISSIONS OWNER_READ GROUP_READ WORLD_READ)
---
+++ src/CMake/modules/FindLibmv.cmake
@@ -0,0 +1,116 @@
+# Locate the Libmv libraries.
+#
+# Defines the following variables:
+#
+#   LIBMV_FOUND        - TRUE if the libmv headers and libs are found
+#   LIBMV_INCLUDE_DIRS - The path to libmv headers
+#
+#   LIBMV_LIBRARIES    - All libmv libraries
+#   LIBMV_LIBRARY_DIR  - The directory where the libraries are located
+#
+# Accepts the following variables as input:
+#
+#   LIBMV_ROOT - (as a CMake or environment variable)
+#                The root directory of the libmv install prefix
+
+MESSAGE(STATUS "Looking for Libmv.")
+
+find_path(LIBMV_INCLUDE_DIR libmv/camera/camera.h
+    HINTS
+    $ENV{LIBMV_ROOT}/include
+    ${LIBMV_ROOT}/include
+    PATH_SUFFIXES
+    libmv
+)
+
+IF(LIBMV_INCLUDE_DIR)
+  MESSAGE(STATUS "Libmv headers found in ${LIBMV_INCLUDE_DIR}")
+ENDIF (LIBMV_INCLUDE_DIR)
+
+SET(LIBMV_LIBRARIES_NAMES camera 
+                          correspondence
+                          daisy
+                          descriptor
+                          detector
+                          fast
+                          flann
+                          gflags
+                          glog
+                          image
+                          jpeg
+                          multiview 
+                          numeric
+                          OpenExif
+                          pthread
+                          png
+                          reconstruction
+                          tools
+                          V3D
+                          zlib)
+
+FIND_LIBRARY(LIBMV_LIBRARY NAMES ${LIBMV_LIBRARIES_NAMES})
+GET_FILENAME_COMPONENT(LIBMV_LIBRARY_DIR "${LIBMV_LIBRARY}" PATH)
+
+IF(LIBMV_LIBRARY)
+  MESSAGE(STATUS "Libmv libraries found: ${LIBMV_LIBRARY}")
+  MESSAGE(STATUS "Libmv libraries directories: ${LIBMV_LIBRARY_DIR}")
+ENDIF (LIBMV_LIBRARY)
+
+SET(LIBMV_LIBRARY "")
+foreach(lib ${LIBMV_LIBRARIES_NAMES})
+  LIST(APPEND LIBMV_LIBRARY ${lib})  
+endforeach()
+
+
+SET(LIBMV_LIBRARIES ${LIBMV_LIBRARY})
+SET(LIBMV_INCLUDE_DIRS ${LIBMV_INCLUDE_DIR})
+
+FIND_PACKAGE(Qt4)
+IF(QT_FOUND)
+  set(LIBMV_LIBRARIES ${LIBMV_LIBRARIES} ${QT_LIBRARIES})
+  set(LIBMV_INCLUDE_DIRS ${LIBMV_INCLUDE_DIRS} ${QT_INCLUDE_DIR})
+  set(LIBMV_LIBRARY_DIRS ${LIBMV_LIBRARY_DIR} ${QT_LIBRARY_DIR})
+ENDIF(QT_FOUND)
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set LIBMV_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(Libmv  DEFAULT_MSG
+                                  LIBMV_LIBRARY LIBMV_INCLUDE_DIR)
+
+mark_as_advanced(LIBMV_INCLUDE_DIR LIBMV_LIBRARY)
+
+IF(LIBMV_FOUND)
+  SET(LIBMV_INCLUDE_DIRS ${LIBMV_INCLUDE_DIRS}
+                         ${LIBMV_INCLUDE_DIR}/third_party/daisy/include
+                         ${LIBMV_INCLUDE_DIR}/third_party/gtest
+                         ${LIBMV_INCLUDE_DIR}/third_party/gtest/include
+                         ${LIBMV_INCLUDE_DIR}/third_party/eigen
+                         ${LIBMV_INCLUDE_DIR}/third_party/ssba
+                         ${LIBMV_INCLUDE_DIR}/third_party/glog/src
+                         ${LIBMV_INCLUDE_DIR}/third_party/OpenExif/src
+                         ${LIBMV_INCLUDE_DIR}/third_party/OpenExif/src/OpenExifJpeg
+  )
+
+  IF (WIN32)
+    INCLUDE_DIRECTORIES(LIBMV_INCLUDE_DIRS ${LIBMV_INCLUDE_DIRS}
+                        ${LIBMV_INCLUDE_DIR}/third_party/msinttypes
+                        ${LIBMV_INCLUDE_DIR}/third_party/jpeg-7
+                        ${LIBMV_INCLUDE_DIR}/third_party/zlib
+                        ${LIBMV_INCLUDE_DIR}/third_party/pthreads-w32/include
+                        ${LIBMV_INCLUDE_DIR}/third_party/png)
+  ENDIF (WIN32)
+
+  IF (APPLE)
+    INCLUDE_DIRECTORIES(LIBMV_INCLUDE_DIRS ${LIBMV_INCLUDE_DIRS}
+                        ${LIBMV_INCLUDE_DIR}/third_party/png
+                        ${LIBMV_INCLUDE_DIR}/third_party/jpeg-7)
+  ENDIF (APPLE)
+
+  IF (UNIX)
+    INCLUDE_DIRECTORIES(LIBMV_INCLUDE_DIRS ${LIBMV_INCLUDE_DIRS}
+                        ${LIBMV_INCLUDE_DIR}/third_party/png
+                        ${LIBMV_INCLUDE_DIR}/third_party/zlib)
+  ENDIF (UNIX)
+ENDIF(LIBMV_FOUND)
+
